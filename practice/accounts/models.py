ef517c08c46830074da243199b9134dc8e0369f3
itut-vl <camifoure@gmail.com>
2026-04-03 21:15:13 +0900

    初回コミット

--- practice/accounts/models.py
+++ practice/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+# models.Modelを継承する
+class Book(models.Model):
+    class Meta:
+        db_table = "book"
+        verbose_name = "本データ"
+
+    title = models.CharField(
+        verbose_name="タイトル",
+        max_length=100,
+        default="タイトル未入力",
+        unique=True,
+    )
+    # fmt: off
+    price = models.IntegerField(
+        verbose_name="値段",
+        null=True,
+        blank=True
+        default=None,
+    )
+    # fmt: on
--- practice/accounts/views.py
+++ practice/accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 
-# Create your views here.
+
+def test_open(request):
+    return TemplateResponse(request, "test.html")
