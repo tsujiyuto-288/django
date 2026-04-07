e37583acccc4f38dcc18a9159b1140d601b655b7
itut-vl <camifoure@gmail.com>
2026-04-07 20:54:33 +0900

    色々修正

--- practice/accounts/views.py
+++ practice/accounts/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
+from accounts.models import (
+    Book,
+    Book_stock,
+    Company,
+    Author
+)
 
 
 def test_open(request):
     return TemplateResponse(request, "test.html")
+
+def create_book(request):
+    pass
+
+def get_model(request):
+    pass
