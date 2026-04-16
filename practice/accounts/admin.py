410c7b1e076dd0bb8dc5b84a7d8079596e5c2298
itut-vl <camifoure@gmail.com>
2026-04-16 20:24:34 +0900

    微修正

--- practice/accounts/admin.py
+++ practice/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, Book_stock, Author, Company
+from accounts.models import Book, Book_stock, Author, Company
 
 admin.site.register(Book)
 admin.site.register(Book_stock)
