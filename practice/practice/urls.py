0247b20b667a2735f8393fd465e96a38642c6eba
itut-vl <camifoure@gmail.com>
2026-05-05 17:02:13 +0900

    new app 作成

--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path("test3/", accounts_views.Test3PageOpen.as_view(), name="test3_open"),
     path("book_list/", accounts_views.BookListView.as_view(), name="book_list_open"),
     path("book/<int:pk>/", accounts_views.BookDetailView.as_view(), name="book_detail_open"),
-    path("mypage/", accounts_views.mypage_open, name="mypage_open"),
     path("book/register/", accounts_views.register_book, name="register_book"),
     path("book/delete/", accounts_views.delete_book, name="delete_book"),
     path("book/get/", accounts_views.get_book, name="get_book"),
