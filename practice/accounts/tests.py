145dc8fda46bea1ec48d42c6e374218516b21a36
itut-vl <camifoure@gmail.com>
2026-05-17 15:11:11 +0900

    gitignore変更

--- practice/accounts/tests.py
+++ practice/accounts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class BookTestCase(TestCase):
+    def test_book_list_page_returns_200(self):
+        url = reverse("book_list_open")
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
